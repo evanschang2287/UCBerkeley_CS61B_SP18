bac59aa5b52cc9ae03aa83949e35cdfeb027ea8b
EvansChang <evanschang2287@gmail.com>
2020-03-20 15:10:00 +0800

    finished proj1B task3

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -8,4 +8,21 @@ public class Palindrome {
 
         return deque;
     }
+
+    public boolean isPalindrome(String word) {
+        if (word.length() == 1 || word.length() == 0) {
+            return true;
+        }
+
+        Palindrome palindrome = new Palindrome();
+        Deque<Character> deque = palindrome.wordToDeque(word);
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (deque.removeFirst() == deque.removeLast()) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
 }
