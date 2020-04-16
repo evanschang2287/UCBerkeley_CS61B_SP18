f1db4bb7895b8b3c4f197727d9c522a7d3ac1e98
EvansChang <evanschang2287@gmail.com>
2020-04-16 14:12:49 +0800

    finished Huffman Encoder

--- hw7/HuffmanEncoder.java
+++ hw7/HuffmanEncoder.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class HuffmanEncoder {
+    public static Map<Character, Integer> buildFrequencyTable(char[] inputSymbols) {
+        Map<Character, Integer> freqTable = new HashMap<>();
+        for (char c : inputSymbols) {
+            if (freqTable.containsKey(c)) {
+                int freq =  freqTable.get(c);
+                freqTable.put(c, freq + 1);
+            } else {
+                freqTable.put(c, 1);
+            }
+        }
+        return freqTable;
+    }
+
+    public static void main(String[] args) {
+        char[] inputSymbols = FileUtils.readFile(args[0]);
+        Map<Character, Integer> freqTable = buildFrequencyTable(inputSymbols);
+        BinaryTrie bt = new BinaryTrie(freqTable);
+
+        ObjectWriter ow = new ObjectWriter(args[0] + ".huf");
+        ow.writeObject(bt);
+
+        Map<Character, BitSequence> lookupTable = bt.buildLookupTable();
+        List<BitSequence> bs = new LinkedList<>();
+
+        for (char c : inputSymbols) {
+            BitSequence bits = lookupTable.get(c);
+            bs.add(bits);
+        }
+
+        BitSequence allBS = BitSequence.assemble(bs);
+        ow.writeObject(allBS);
+    }
 }
